76b7d6232caacf1cb76558068af3a28b489e2959
mattjmcnaughton <mattjmcnaughton@gmail.com>
2018-03-15 19:44:25 -0400

    Bump version to 0.1.0
    0.1.0 symbolizes a working MVP.
    
    Additionally, fix the `version` subcommand.

--- cmd/version.go
+++ cmd/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "0.0.1"
+const version = "0.1.0"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
@@ -14,4 +14,8 @@ var versionCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("%s\n", version)
 	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
 }
